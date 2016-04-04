e7782ad3238790de2abb4588cab882951b41a560
molleolson <molle.olson@gmail.com>
2016-04-04 15:34:27 +0200

    molle testar

--- back-end/settings.py
+++ back-end/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'back-end.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'user_data',
+        'ENGINE': 'mysql.connector.django',
+        'USER': 'mysql_user',
+        'PASSWORD': 'password',
+        'OPTIONS': {
+          'autocommit': True,
+        },
     }
 }
 
