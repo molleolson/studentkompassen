c03eb82f8e48feed1637f99d832a90acf07d6e33
soma6970 <soma6970@student.uu.se>
2016-04-05 15:14:39 +0200

    Tar fram "Just nu" och gömmer vid klick

--- Front-End/functions.js
+++ Front-End/functions.js
@@ -1,3 +1,26 @@
 /**
  * Created by Sofie on 2016-04-05.
  */
+
+$(document).ready(function(){
+    $('#btnClick').on('click',function(){
+        if($('#1').css('display')!='none'){
+            $('#2').show();
+            $('#1').hide();
+        }else if($('#2').css('display')!='none'){
+            $('#1').show();
+            $('#2').hide();
+        }
+    });
+});
+$(document).ready(function(){
+    $('#justnuklick').on('click',function(){
+        if($('#middleboxJustnu').css('display')!='none'){
+
+            $('#middleboxJustnu').hide();
+        }else {
+            $('#middleboxJustnu').show();
+
+        }
+    });
+});
