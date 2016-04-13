7d2bcb12f188668e636d6678b3ecbde6ee5d2da7
adrian norrby <adrian.norrby@gmail.com>
2016-04-13 10:55:20 +0200

    Merge remote-tracking branch 'origin/master'

--- start/models.py
+++ start/models.py
@@ -1,7 +1,28 @@
 from __future__ import unicode_literals
+import datetime
 
-
-
+from django.utils import timezone
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+class Host(models.Model):
+    host_name = models.CharField(max_length=200)
+    host_email = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.host_name
+
+class Event(models.Model):
+    event_name = models.CharField(max_length=200)
+    event_description = models.CharField(max_length=400)
+    event_date = models.CharField(max_length=200)
+    event_start = models.CharField(max_length=200)
+    event_end = models.CharField(max_length=200)
+
+class Lunch(Event):
+
+
+#class Location(models.Model):
+
+
+#class Event(models.Model):
--- start/views.py
+++ start/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Host
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    list_of_hosts = Host.objects.order_by('id')
+    output = ', '.join([h.host_name for h in list_of_hosts])
+    return HttpResponse(output)
 # Create your views here.
+def hostid(request, id):
+    return HttpResponse("You're looking at host %s." % id)
+
+
+
