ea587399fcda3f5797ef665d8ac966bd47135d4a
AbbeUser <al.catalan88@gmail.com>
2016-04-14 14:57:59 +0200

    Städade funktioner och transistion på flikar. /Sofie

---
+++ Front-End/datefunctions.js
@@ -0,0 +1,10 @@
+/**
+ * Created by abbe on 2016-04-14.
+ */
+
+$(document).ready(function() {
+
+    $('#sandbox-container .input-group.date').datepicker({
+        calenderWeeks: true
+    });
+});
