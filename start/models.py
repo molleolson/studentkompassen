b4a2d72ee554c06b96620df13b5e61b79c616946
adrian norrby <adrian.norrby@gmail.com>
2016-04-14 15:54:25 +0200

    Merge remote-tracking branch 'origin/master'

--- start/models.py
+++ start/models.py
@@ -7,19 +7,31 @@ from django.utils.encoding import python_2_unicode_compatible
 
 
 class Host(models.Model):
-    host_name = models.CharField(max_length=200)
-    host_email = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
 
     def __unicode__(self):
-        return self.host_name
+        return self.name
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=200, blank=True, null=True)
+    gps = models.CharField(max_length=200, blank=True, null=True)
+    street = models.CharField(max_length=200, blank=True, null=True)
+    streetnumber = models.IntegerField(blank=True, null=True)
+    zip = models.IntegerField(blank=True, null=True)
+    city = models.CharField(max_length=200, blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
 
 
 class Event(models.Model):
-    event_name = models.CharField(max_length=200, blank=True, null=True)
-    event_description = models.CharField(max_length=400, blank=True, null=True)
-    event_startdate = models.DateTimeField('start date')
-    event_enddate = models.DateTimeField('end date')
-    event_location = models.ForeignKey('Location', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    description = models.CharField(max_length=400, blank=True, null=True)
+    startdate = models.DateTimeField('start date', auto_now_add=True)
+    enddate = models.DateTimeField('end date', auto_now_add=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True)
     # event_start_day = models.CharField(max_length=200, blank=True, null=True)
     # Kanske ska va datum och ej string?
     # event_start_month = models.CharField(max_length=200, blank=True, null=True)
@@ -33,29 +45,17 @@ class Event(models.Model):
     #   event_participants = BooleanField(required = False)
 
     def __unicode__(self):
-        return self.event_name
+        return self.name
 
 
 class Lunch(Event):
-    lunch_menu = models.CharField(max_length=200) # Hr kanske vi sen kan testa lada in pdf?
+    menu = models.CharField(max_length=200) # Hr kanske vi sen kan testa lada in pdf?
 
 
 class Other(Event):
     pass
 
 
-class Location(models.Model):
-    location_name = models.CharField(max_length=200, blank=True, null=True)
-    location_gps = models.CharField(max_length=200, blank=True, null=True)
-    location_street = models.CharField(max_length=200, blank=True, null=True)
-    location_streetnumber = models.IntegerField(blank=True, null=True)
-    location_zip = models.IntegerField(blank=True, null=True)
-    location_city = models.CharField(max_length=200, blank=True, null=True)
-
-    def __unicode__(self):
-        return self.event_name
-
-
-class Admin(models.Model):
-    admin_usrname = models.CharField(max_length=200)
+#class Administrator(models.Model):
+#    username = models.CharField(max_length=200)
 #class Event(models.Model):
