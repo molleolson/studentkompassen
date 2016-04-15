b7a310b3319c1b6522cd986339e4a72acf8a61ee
soma6970 <soma6970@student.uu.se>
2016-04-15 15:49:49 +0200

    Update dateAndTime.js

--- Front-End/dateAndTime.js
+++ Front-End/dateAndTime.js
@@ -3,43 +3,43 @@
  */
 $ (function() {
     var d = new Date();
-var weekday = new Array(7);
-weekday[0] = "Sunday";
-weekday[1] = "Monday";
-weekday[2] = "Tuesday";
-weekday[3] = "Wednesday";
-weekday[4] = "Thursday";
-weekday[5] = "Friday";
-weekday[6] = "Saturday";
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
 
 
-var n = weekday[d.getDay()];
-document.getElementById("getDay").innerHTML = n;
+    var n = weekday[d.getDay()];
+    document.getElementById("getDay").innerHTML = n;
 })
 
 $ (function(){
     var p = new Date();
-var q = p.getDate();
-document.getElementById("getDate").innerHTML = q;
+    var q = p.getDate();
+    document.getElementById("getDate").innerHTML = q;
 })
 
 $ (function() {
     var a = new Date();
-var month = new Array(12);
+    var month = new Array(12);
 
-month[0] = "January";
-month[1] = "February";
-month[2] = "Mars";
-month[3] = "April";
-month[4] = "May";
-month[5] = "June";
-month[6] = "July";
-month[7] = "August";
-month[8] = "September";
-month[9] = "October";
-month[10] = "November";
-month[11] = "December";
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "Mars";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
 
-var b = month[a.getMonth()];
-document.getElementById("getMonth").innerHTML = b;
+    var b = month[a.getMonth()];
+    document.getElementById("getMonth").innerHTML = b;
 })
