076be2c47d8ade2a4908db629ab45d6be0418089
adrian norrby <adrian.norrby@gmail.com>
2016-04-15 10:56:38 +0200

    Merge remote-tracking branch 'origin/master'

--- start/admin.py
+++ start/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Host, Event, Location
+from .models import Host, Event, Location, Lunch
 
 admin.site.register(Host)
 admin.site.register(Event)
 admin.site.register(Location)
+admin.site.register(Lunch)
 
 # Register your models here.
