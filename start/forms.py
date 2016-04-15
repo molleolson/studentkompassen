d0ab62a269897dfcd9e4b924fef4a91e14117d86
adrian norrby <adrian.norrby@gmail.com>
2016-04-15 13:51:34 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ start/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.forms import inlineformset_factory, ModelForm
+from start.models import Event, Location
+
+
+class LocationForm(ModelForm):
+    class Meta:
+        model = Location
+        fields = '__all__'
+
+
+EventFormset = inlineformset_factory(Location, Event, fields=('name', 'description', 'startdate', 'enddate'), extra=1, can_delete=False)
+
+
