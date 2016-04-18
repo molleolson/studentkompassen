75ebe27d30061c7d5e67e03486c2ef641a8a7b6d
adrian norrby <adrian.norrby@gmail.com>
2016-04-18 12:50:02 +0200

    Merge remote-tracking branch 'origin/master'

--- start/urls.py
+++ start/urls.py
@@ -5,8 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^nationmain/$', views.nationmain, name='nationmain'),
-    url(r'^(?P<id>[0-9]+)/skapa/$', views.skapa, name='skapa'),
-    url(r'^addevent/$', views.addevent, name='addevent'),
+    url(r'^nationmain/addevent/$', views.addevent, name='addevent'),
 
 
 ]
