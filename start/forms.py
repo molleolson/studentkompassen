7fc622e44ebd74baeb53bace1f03aecf8789f4c2
adrian norrby <adrian.norrby@gmail.com>
2016-04-18 17:07:30 +0200

    Merge remote-tracking branch 'origin/master'

--- start/forms.py
+++ start/forms.py
@@ -2,33 +2,17 @@ from django import forms
 from django.forms import inlineformset_factory, ModelForm, DateField, TextInput
 from start.models import Event, Location, Host
 from functools import partial
+from datetimewidget.widgets import DateTimeWidget
 
-
-
-#class LocationForm(ModelForm):
-#    class Meta:
-#        model = Location
- #       fields =('name', )
-
-
-#EventFormset = inlineformset_factory(Location, Event, fields=('name', 'description', 'startdate',
-#                                                              'enddate'), extra=1, can_delete=False)
 
 class EventForm(ModelForm):
-    #startdate = DateField(widget=TextInput(attrs={'class':'datetimepicker'}))
-    #enddate = DateField(widget=TextInput(attrs={'class': 'datetimepicker'}))
-
     class Meta:
         model = Event
         fields = ['name', 'description','startdate', 'enddate', 'host', 'location']
         widgets = {
-            'startdate': forms.DateTimeInput(attrs={'class':'datetimepicker'}),
-            'enddate': forms.DateTimeInput(attrs={'class': 'datetimepicker'}),
-
+            # Use localization and bootstrap 3
+            'startdate': DateTimeWidget(attrs={'class': "datetimepicker"}, usel10n=True, bootstrap_version=3),
+            'enddate': DateTimeWidget(attrs={'class': "datetimepicker"}, usel10n=True, bootstrap_version=3)
         }
-
-       # widget=forms.DateTimeInput
-        #startdate = widget
-        #enddate = widget
 
 
