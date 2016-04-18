ad526a09a3fa6f2cf216b080f6bd7332e8204601
EbbaUser <ebbaholmstrom@hotmail.com>
2016-04-18 14:11:55 +0200

    ww

--- start/forms.py
+++ start/forms.py
@@ -3,6 +3,8 @@ from django.forms import inlineformset_factory, ModelForm
 from start.models import Event, Location, Host
 from functools import partial
 
+
+
 #class LocationForm(ModelForm):
 #    class Meta:
 #        model = Location
@@ -12,12 +14,11 @@ from functools import partial
 #EventFormset = inlineformset_factory(Location, Event, fields=('name', 'description', 'startdate',
 #                                                              'enddate'), extra=1, can_delete=False)
 
-DateInput= partial(forms.DateTimeInput, {'class':'datepicker'})
 class EventForm(ModelForm):
     class Meta:
         model = Event
-        fields = '__all__'
-
-        startdate = forms.DateTimeInput(widgets=forms.DateTimeInput())
-        enddate = forms.DateTimeInput(widgets=forms.DateTimeInput())
+        fields = ['startdate', 'enddate']
+       # widget=forms.DateTimeInput
+        #startdate = widget
+        #enddate = widget
 
