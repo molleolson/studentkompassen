6ffb56383fd59d67c9a72428a91bf71e4f05c92c
adrian norrby <adrian.norrby@gmail.com>
2016-04-19 15:08:50 +0200

    Merge remote-tracking branch 'origin/master'

--- start/static/start/calender.js
+++ start/static/start/calender.js
@@ -3,4 +3,4 @@
  */
 $(function() {
     $( "#datepicker" ).datepicker();
-});
+  });
--- start/views.py
+++ start/views.py
@@ -12,7 +12,7 @@ def index(request):
 
     #template = loader.get_template('start/main.html')
     #return HttpResponse(template.render(request))
-    return render(request, 'start/studentmain.html', locals())
+    return render(request, 'start/main.html', locals())
 
 
 #    
@@ -33,6 +33,7 @@ def skapa(request, id):
 
 def nationmain(request):
     menu_active_item = 'now'
+    events = Event.objects.all()
     return render(request, 'start/nationmain.html', locals())
 
 
