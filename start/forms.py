6fafde580f17306db7cb5acf0e750ab6822404c8
EbbaUser <ebbaholmstrom@hotmail.com>
2016-04-19 09:39:35 +0200

    ww

--- start/forms.py
+++ start/forms.py
@@ -11,8 +11,10 @@ class EventForm(ModelForm):
         fields = ['name', 'description','startdate', 'enddate', 'host', 'location']
         widgets = {
             # Use localization and bootstrap 3
-            'startdate': DateTimeWidget(attrs={'class': "datetimepicker"}, usel10n=True, bootstrap_version=3),
-            'enddate': DateTimeWidget(attrs={'class': "datetimepicker"}, usel10n=True, bootstrap_version=3)
+            'startdate': DateTimeWidget(attrs={'class': "multidatespicker"}, usel10n=True, bootstrap_version=3),
+            'enddate': DateTimeWidget(attrs={'class': "datetimepicker"}, usel10n=True, bootstrap_version=3),
+            'multipledates': DateTimeWidget(attrs={'class': "multidatespicker"}, usel10n=True, bootstrap_version=3),
+
         }
 
 
