e18d0583d1aef456b5f9e781ea33684a9d208d1c
AbbeUser <al.catalan88@gmail.com>
2016-05-03 14:06:20 +0200

    -.-

--- start/forms.py
+++ start/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.forms import inlineformset_factory, ModelForm, DateField, TextInput
+from django.forms import inlineformset_factory, ModelForm, Textarea, DateField,CheckboxSelectMultiple, TextInput, MultipleChoiceField
 from start.models import Event, Location, Host
 from functools import partial
 from datetimewidget.widgets import DateTimeWidget
@@ -9,14 +9,19 @@ from datetimewidget.widgets import DateTimeWidget
 class EventForm(ModelForm):
     class Meta:
         model = Event
-        fields = ['name', 'description', 'startdate', 'enddate', 'categories', 'host', 'location']
+        fields = ['name', 'categories','host', 'location', 'startdate', 'enddate', 'description']
 
         widgets = {
             # Use localization and bootstrap 3
             'startdate': DateTimeWidget(attrs={'class': "datetimepicker"}, usel10n=True, bootstrap_version=3),
-            'enddate': DateTimeWidget(attrs={'class': "datetimepicker"}, usel10n=True, bootstrap_version=3)
+            'enddate': DateTimeWidget(attrs={'class': "datetimepicker"}, usel10n=True, bootstrap_version=3),
             #'multipledates': DateTimeWidget(attrs={'class': "multidatespicker"}, usel10n=True, bootstrap_version=3),
+            'categories': CheckboxSelectMultiple(attrs={'class': "MultipleChoiceField"}),
+            'description': Textarea(attrs={'cols': 60, 'rows': 10})
         }
+
+        #MultipleChoiceField(required=True, widget=CheckboxSelectMultiple, choices='categories')
+
 
 
 class PresentationForm(ModelForm):
