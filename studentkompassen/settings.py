a4da2d871ff9918eeecde6cd3100c49df43861a9
adrian norrby <adrian.norrby@gmail.com>
2016-05-10 10:27:40 +0200

    Merge remote-tracking branch 'origin/master'

--- studentkompassen/settings.py
+++ studentkompassen/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'datetimewidget.widgets',
+    'recurrence',
     'studentkompassen',
     'django_social_app',
     'social.apps.django_app.default',
