b8f8130f8121a11cee33a114e878a97a2c25cc7c
AbbeUser <al.catalan88@gmail.com>
2016-05-10 15:47:31 +0200

    Jäkla addeventkrångel

--- start/forms.py
+++ start/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.forms import inlineformset_factory, ModelForm, Textarea, DateField,CheckboxSelectMultiple, TextInput, MultipleChoiceField
 from start.models import Event, Location, Host
-from django.forms import inlineformset_factory, ModelForm, CharField, DateField, TextInput
+from django.forms import inlineformset_factory, ModelForm,RadioSelect,MultiWidget, CharField, DateField, TextInput
 from start.models import Event, Host
 from functools import partial
 from datetimewidget.widgets import DateTimeWidget
@@ -18,7 +18,7 @@ class EventForm(ModelForm):
             'startdate': DateTimeWidget(attrs={'class': "datetimepicker"}, usel10n=True, bootstrap_version=3),
             'enddate': DateTimeWidget(attrs={'class': "datetimepicker"}, usel10n=True, bootstrap_version=3),
             #'multipledates': DateTimeWidget(attrs={'class': "multidatespicker"}, usel10n=True, bootstrap_version=3),
-            'categories': CheckboxSelectMultiple(attrs={'class': "MultipleChoiceField"}),
+            'categories': CheckboxSelectMultiple(attrs={'class': 'choice_field'}),
             'description': Textarea(attrs={'cols': 60, 'rows': 10}),
             'name': TextInput(attrs={'class': "nameofevent", 'rows':1})
         }
